4e6b1811a2646850ba05628a83025de677857389
EreticKB <59476878+EreticKB@users.noreply.github.com>
2021-12-18 02:07:33 +0300

    Backup

--- First's Block Exam/Assets/_Project/Scripts/Body.cs
+++ First's Block Exam/Assets/_Project/Scripts/Body.cs
@@ -5,22 +5,17 @@ public class Body : MonoBehaviour
 
     //Возможно скрипт полностью устарел.
     public Transform PreviousSegment;
-    private Vector3 _transformBuffer;
     private float _timeDelay;
+    public float Speed;
 
-    private void Awake()
-    {
-        _transformBuffer = PreviousSegment.position;
-    }
     public void Movement()
         {
         if (_timeDelay < 0.1) _timeDelay += Time.deltaTime;
         else
         {
             _timeDelay = 0;
-            transform.position = _transformBuffer;
-            _transformBuffer = PreviousSegment.position;
-
+            transform.LookAt(PreviousSegment);
+            transform.Translate(Vector3.forward * Time.deltaTime * Speed, Space.Self);
         }
 
         }
