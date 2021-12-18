920f4245ecc8c23f1f08a6c725a03fdab4cde959
EreticKB <59476878+EreticKB@users.noreply.github.com>
2021-12-18 03:42:20 +0300

    Nah, need break

--- First's Block Exam/Assets/_Project/Scripts/Body.cs
+++ First's Block Exam/Assets/_Project/Scripts/Body.cs
@@ -3,23 +3,34 @@ using UnityEngine;
 public class Body : MonoBehaviour
 {
 
-    //Возможно скрипт полностью устарел.
+    
     public Transform PreviousSegment;
     private Vector3 _transformBuffer;
-    private float _timeDelay;
     private Rigidbody _thisSegment;
     public float Speed;
-    public int TargetDistance;
+    public float TargetDistance;
 
     private void Awake()
     {
         _transformBuffer = PreviousSegment.position;
         _thisSegment = GetComponent<Rigidbody>();
+        
     }
     private void FixedUpdate()
     {
-        transform.LookAt(_transformBuffer);
-        _thisSegment.velocity = Vector3.forward * Speed * Time.deltaTime;
-        if ((transform.position - _transformBuffer).magnitude < TargetDistance) _transformBuffer = PreviousSegment.position;
+        float distance = Mathf.Abs((transform.position - _transformBuffer).sqrMagnitude);
+        if (distance < TargetDistance)
+        {
+            _transformBuffer = PreviousSegment.position;
+            _thisSegment.velocity = Vector3.forward * 0;
+            //Debug.Log("========="+ distance);
+        }
+        else
+        {
+            transform.LookAt(_transformBuffer);
+            _transformBuffer = PreviousSegment.position;
+            _thisSegment.velocity = transform.forward * Speed * Time.deltaTime;
+            //Debug.Log(distance);
+        }
     }
 }
